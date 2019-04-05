6ab2b5ed902691e94c263f398dc964c12c35da7e
MELİSA ECE SURAL <47081681+MelisaEceSural@users.noreply.github.com>
2019-04-05 18:36:10 +0300

    Add files via upload

---
+++ 20190218_disconnectedMimari/20190218_disconnectedMimari/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace _20190218_disconnectedMimari
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.cmbKategori = new System.Windows.Forms.ComboBox();
+            this.cmbTedarikci = new System.Windows.Forms.ComboBox();
+            this.numAdet = new System.Windows.Forms.NumericUpDown();
+            this.numFiyat = new System.Windows.Forms.NumericUpDown();
+            this.txtAdi = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grdUrunler = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numAdet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numFiyat)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdUrunler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1117, 462);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnEkle);
+            this.tabPage1.Controls.Add(this.cmbKategori);
+            this.tabPage1.Controls.Add(this.cmbTedarikci);
+            this.tabPage1.Controls.Add(this.numAdet);
+            this.tabPage1.Controls.Add(this.numFiyat);
+            this.tabPage1.Controls.Add(this.txtAdi);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.grdUrunler);
+            this.tabPage1.Location = new System.Drawing.Point(4, 26);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1109, 432);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Ürünler";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1109, 436);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Kategoriler";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(308, 382);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(121, 32);
+            this.btnEkle.TabIndex = 17;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            // 
+            // cmbKategori
+            // 
+            this.cmbKategori.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbKategori.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbKategori.FormattingEnabled = true;
+            this.cmbKategori.Location = new System.Drawing.Point(308, 305);
+            this.cmbKategori.Name = "cmbKategori";
+            this.cmbKategori.Size = new System.Drawing.Size(121, 28);
+            this.cmbKategori.TabIndex = 15;
+            // 
+            // cmbTedarikci
+            // 
+            this.cmbTedarikci.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTedarikci.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbTedarikci.FormattingEnabled = true;
+            this.cmbTedarikci.Location = new System.Drawing.Point(308, 342);
+            this.cmbTedarikci.Name = "cmbTedarikci";
+            this.cmbTedarikci.Size = new System.Drawing.Size(121, 28);
+            this.cmbTedarikci.TabIndex = 16;
+            // 
+            // numAdet
+            // 
+            this.numAdet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numAdet.Location = new System.Drawing.Point(89, 385);
+            this.numAdet.Name = "numAdet";
+            this.numAdet.Size = new System.Drawing.Size(120, 26);
+            this.numAdet.TabIndex = 13;
+            // 
+            // numFiyat
+            // 
+            this.numFiyat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numFiyat.Location = new System.Drawing.Point(89, 344);
+            this.numFiyat.Name = "numFiyat";
+            this.numFiyat.Size = new System.Drawing.Size(120, 26);
+            this.numFiyat.TabIndex = 14;
+            // 
+            // txtAdi
+            // 
+            this.txtAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAdi.Location = new System.Drawing.Point(89, 305);
+            this.txtAdi.Name = "txtAdi";
+            this.txtAdi.Size = new System.Drawing.Size(120, 26);
+            this.txtAdi.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(17, 385);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Adet";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(236, 345);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Tedarikçi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(236, 308);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Kategori";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(17, 344);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Fiyat";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(17, 308);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Ürün Adı";
+            // 
+            // grdUrunler
+            // 
+            this.grdUrunler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdUrunler.Dock = System.Windows.Forms.DockStyle.Top;
+            this.grdUrunler.Location = new System.Drawing.Point(3, 3);
+            this.grdUrunler.Name = "grdUrunler";
+            this.grdUrunler.Size = new System.Drawing.Size(1103, 298);
+            this.grdUrunler.TabIndex = 6;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 26);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1109, 432);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Tedarikçiler";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 462);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numAdet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numFiyat)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdUrunler)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ComboBox cmbKategori;
+        private System.Windows.Forms.ComboBox cmbTedarikci;
+        private System.Windows.Forms.NumericUpDown numAdet;
+        private System.Windows.Forms.NumericUpDown numFiyat;
+        private System.Windows.Forms.TextBox txtAdi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView grdUrunler;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+    }
+}
+
---
+++ 20190218_disconnectedMimari/20190218_disconnectedMimari/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace _20190218_disconnectedMimari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Server=.;Database=Northwind2;Integrated Security=true");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getUrunler();
+        }
+
+        private void getUrunler()
+        {
+            
+
+            SqlDataAdapter adp = new SqlDataAdapter("select * from Urunler", baglanti);
+            // Eğer sorgu sonucunda tek bir tablo dönüyorsa DataTable nesnesini kullanabiliriz.
+            DataTable dt = new DataTable();
+            //Sorgudan dönen tabloyu datatable nesnesibe aktaralım
+            adp.Fill(dt);
+            //grid'i datatable ile dolduralım
+            grdUrunler.DataSource = dt;
+
+            // benim 2 comboya ilgili verileri yüklemem lazım
+            SqlDataAdapter adpKatTed = new SqlDataAdapter("Select * from Kategoriler Select * from Tedarikciler", baglanti);
+
+            DataSet ds = new DataSet();// Dataset içinde birden fazla datatable bulunduran nesnedir. Sorgu sonucunda birden fazla tablo döneceği için bu tabloları datatable ile değil dataset ile karşılıyorum.
+            adpKatTed.Fill(ds);
+
+            cmbKategori.DataSource = ds.Tables[0];// Kategoriler
+            cmbKategori.DisplayMember = "KategoriAdi";// Kategori Adı gözüksün
+            cmbKategori.ValueMember = "KategoriID";// KategoriId ile işlem yapılsın
+
+
+            cmbTedarikci.DataSource = ds.Tables[1];//Tedarikçiler
+            cmbTedarikci.DisplayMember = "SirketAdi";
+            cmbTedarikci.ValueMember = "TedarikciID";
+
+            txtAdi.Text = "";
+            numAdet.Value = numFiyat.Value = 0;
+            cmbKategori.SelectedIndex = cmbTedarikci.SelectedIndex = 0;
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtAdi.Text) || numAdet.Value == 0 || numFiyat.Value == 0)
+            {
+                MessageBox.Show("Lütfen ürün bilgilerini boş geçmeyin.");
+                return;
+            }
+
+            SqlCommand cmd = new SqlCommand("Insert into Urunler(UrunAdi, BirimFiyati, HedefStokDuzeyi, KategoriID, TedarikciID) values(@Adi, @Fiyat, @Stok, @Kat, @Ted)", baglanti);
+            cmd.Parameters.AddWithValue("@Adi", txtAdi.Text);
+            cmd.Parameters.AddWithValue("@Fiyat", numFiyat.Value);
+            cmd.Parameters.AddWithValue("@Stok", numAdet.Value);
+            cmd.Parameters.AddWithValue("@Kat", cmbKategori.SelectedValue);
+            cmd.Parameters.AddWithValue("@Ted", cmbTedarikci.SelectedValue);
+            baglanti.Open();
+            int s = cmd.ExecuteNonQuery();
+            baglanti.Close();
+            if (s != 0) MessageBox.Show("Ürün eklendi.");
+            else MessageBox.Show("Ürün eklenirken hata oluştu.");
+
+            getUrunler();
+
+        }
+    }
+}
